3a692f8663502ab67f015e2b80f89923c551792e
alaa <alatothman110@gmail.com>
2023-07-03 15:51:11 +0100

    okey

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import "../css/notfound.css";
 function Notfound() {
   return (
-    <div><h1>404</h1></div>
-  )
+    <div className="not">
+      <div class="page-wrap d-flex flex-row align-items-center found">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+              <span class="display-1 d-block">404</span>
+              <div class="mb-4 lead">
+                The page you are looking for was not found.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Notfound
+export default Notfound;
